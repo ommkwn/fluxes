399410661e2efaf41cf1571a8c5fe03ed993f1f6
ommkwn <ommkwn2001@gmail.com>
2025-04-02 17:36:54 +0530

    Added Helm chart and configuration files for Flux

--- FluxConsole/Program.cs
+++ FluxConsole/Program.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, om!");
 Console.WriteLine("Hello, krn!");
@@ -7,3 +8,10 @@ Console.WriteLine("Hello, hnn!");
 
 
 
+=======
+﻿Console.WriteLine("Hello, om!");
+Console.WriteLine("Hello, krn!");
+Console.WriteLine("Hello, fur!");
+Console.WriteLine("Hello, hm!");
+Console.WriteLine("Hello, hnn!");
+>>>>>>> 0cba7c5 (Added Helm chart and configuration files for Flux)
